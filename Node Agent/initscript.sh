dbf37348c893a330f63235c7faf52885a9f23fea
Shyam G. Pradhan <2018bcs509@sggs.ac.in>
2025-02-13 19:41:14 +0530

    Update initscript.sh
    Was failing with shell script issue

--- Node Agent/initscript.sh
+++ Node Agent/initscript.sh
@@ -4,4 +4,4 @@ echo "Ading agent-user in sudo group..."
 sudo usermod -aG sudo agent-user
 echo "Setting up the agent-user to use without password..."
 sudo visudo
-agent-user ALL=(ALL) NOPASSWD:ALL
+echo 'agent-user ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/agent-user
