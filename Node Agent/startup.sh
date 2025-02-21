25e758000468a237ea208970c76815ddf52d3083
shyamprgrmr <2018bcs509@sggs.ac.in>
2025-02-21 19:29:30 +0530

    startup.sh

--- Node Agent/startup.sh
+++ Node Agent/startup.sh
@@ -1,7 +1,7 @@
 
 mkdir -p /tmp/app/
 curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/nginx/nginx.conf > /tmp/app/nginx.conf
-curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/prometheus/prmoetheus.yaml > /tmp/app/prometheus.yaml
+curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/prometheus/prmoetheus.yaml > /tmp/app/prometheus.yml
 curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/docker-compose.yaml > docker-compose.yaml
 
 docker-compose up -d
