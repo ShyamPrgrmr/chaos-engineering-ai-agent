e52ba221d9ae31bdc1f3ef91fdf1a60cb6de071e
shyamprgrmr <2018bcs509@sggs.ac.in>
2025-02-22 17:08:07 +0530

    Final touches to docker-compose and nginx conf

--- Node Agent/startup.sh
+++ Node Agent/startup.sh
@@ -4,5 +4,5 @@ curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/re
 curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/prometheus/prmoetheus.yaml > /tmp/app/prometheus.yml
 curl https://raw.githubusercontent.com/ShyamPrgrmr/chaos-engineering-ai-agent/refs/heads/main/Node%20Agent/docker-compose.yaml > docker-compose.yaml
 
-docker-compose up -d
+
 
